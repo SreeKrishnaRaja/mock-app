b7499c8fbfaa934cce36f4e1ec1c650faae5d8c7
Sree Krishna Raja K <sreekrishnarajak@gmail.com>
2019-07-01 20:58:17 +0530

    Feature: Pagination
    * Added Pagination in Home Page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -15,7 +15,9 @@ export default class Home extends React.PureComponent {
   state = {
     redirect: false,
     posts: [],
-    data: {}
+    data: {},
+    page: 1,
+    inProgress: false
   }
 
   componentDidMount() {
@@ -24,49 +26,59 @@ export default class Home extends React.PureComponent {
 
   componentDidUpdate(prevProps) {
     if (prevProps.path !== this.props.path) {
-      this.setState({ posts: [], data: {} });
+      this.setState({ posts: [], data: {}, page: 1 });
       this.getData();
     }
   }
 
   getData = () => {
     const { path, id } = this.props;
+    const { page } = this.state;
     const token = localStorage.getItem('token');
     const savedThis = this;
     if (!token) {
       this.setState({ redirect: true });
     } else {
       if (path === home) {
+        this.setState({ inProgress: true });
         axios.post('/posts', {
           token: token,
+          page: page
         })
         .then(function (response) {
-          return response.data.data;
+          return response.data;
         })
         .then(function (response) {
-          savedThis.setState({ posts: response });
+          savedThis.setState({ posts: savedThis.state.posts.concat(response.data), inProgress: false, page: response.nextPage });
         })
+        .catch(function (error) {
+          savedThis.setState({ inProgress: false });
+        });
       } else if (path === post && id) {
         axios.post('/post', {
           token: token,
           id: this.props.id
         })
         .then(function (response) {
-          return response.data.data;
+          return response.data;
         })
         .then(function (response) {
-          savedThis.setState({ data: response });
+          savedThis.setState({ data: response.data, inProgress: false });
         })
+        .catch(function (error) {
+          savedThis.setState({ inProgress: false });
+        });
       }
     }
   }
 
   render() {
-    const { redirect, posts, data } = this.state;
+    const { redirect, posts, data, page, inProgress } = this.state;
     const { path } = this.props;
     if (redirect) {
       navigate('/login');
     }
+    console.log(this.state);
     return (
       <div className="wrapper">
         <Header />
@@ -80,6 +92,16 @@ export default class Home extends React.PureComponent {
               {posts.length === 0 &&
                 <div className="loader">
                   <Loading type='spinningBubbles' color="#0073b1c9" height={100} width={100} />
+                </div>}
+              {posts.length > 0 && page &&
+                <div className="loader">
+                  {!inProgress && <button
+                    className="button-show-more"
+                    onClick={this.getData}
+                  >
+                    Show More
+                  </button>}
+                  {inProgress && <Loading type='spinningBubbles' color="#0073b1c9" height={50} width={50} />}
                 </div>}
             </div>}
           {path === post &&
